e5c6d410d2f36917dcd78dad4ef0b69b26565658
RadoslawP <radoslaw.pietraszek.r@gmail.com>
2020-06-09 18:28:26 +0200

    wycinanie w stringach

---
+++ wycinanie_string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string napis="Ala ma kota";
+    napis.erase(3,3);
+
+    cout <<napis<< endl;
+    return 0;
+}
